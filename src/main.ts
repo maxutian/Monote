a24314f63b876403d828da3a7559518ce5a1f149
马旭天 <maxutian95@gmail.com>
2020-12-24 19:55:04 +0800

    basic layout

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Home from './pages/home/index.vue';
+import './main.less';
+
+createApp(Home).mount('#app')
---
+++ src/pages/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center pt-24 w-full h-screen" :class="prefix">
+    <div class="w-3/12 mx-2 bg-white"></div>
+    <div class="w-5/12 mx-2 bg-white"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup: () => {
+    const prefix = ref('home');
+
+    return {
+      prefix
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+</style>
