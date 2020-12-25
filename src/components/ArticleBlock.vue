b418a5e261cf19f238a4f28134e1a99484c544f9
马旭天 <maxutian95@gmail.com>
2020-12-25 18:12:18 +0800

    homepage basic style

---
+++ src/components/ArticleBlock.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex border-b border-yellow-300 px-6 py-4 justify-between items-center cursor-pointer" :class="prefix">
+    <p class=" text-gray-600 text-lg flex-1 hover:text-gray-900">{{article.title}}</p>
+    <p class=" text-gray-400 text-sm">#{{article.index}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, PropType, defineComponent } from 'vue';
+
+interface Article {
+  id: string;
+  title: string;
+  index: number;
+}
+
+const ArticleBlock = defineComponent({
+  props: {
+    article: {
+      type: Object as PropType<Article>,
+      required: true,
+    }
+  },
+  setup: () => {
+    const prefix = ref<string>('article-block');
+
+    return {
+      prefix,
+    }
+  }
+})
+
+export default ArticleBlock;
+</script>
+
+<style lang="less">
+
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="flex justify-center pt-24 w-full h-screen" :class="prefix">
-    <div class="w-3/12 mx-2 bg-white"></div>
-    <div class="w-5/12 mx-2 bg-white"></div>
+    <div class="w-3/12 mx-2 pl-6 pr-16">
+      <p class="text-right text-4xl">Monote</p>
+      <p class="break-words text-right text-white my-7">Don't aim for success if you want it; just do what you love and believe in, and it will come naturally.</p>
+      <ul class="text-right">
+        <li class="nav-item">Home</li>
+        <li class="nav-item">Tech</li>
+        <li class="nav-item">Life</li>
+        <li class="nav-item">Resume</li>
+      </ul>
+    </div>
+    <div class="w-5/12 mx-2 bg-white shadow-lg rounded-t-md">
+      <article-block v-for="article in articleList" :key="article.id" :article="article"></article-block>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { ref } from 'vue';
+import ArticleBlock from '@/components/ArticleBlock.vue';
 
-export default defineComponent({
+export default {
   setup: () => {
     const prefix = ref('home');
+    const articleList = ref([{ id: 'fwegweg', title: 'My First Article', index: 1 }]);
 
     return {
-      prefix
+      prefix,
+      articleList,
     }
-  }
-})
+  },
+  components: { "article-block": ArticleBlock }
+}
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+  .nav-item {
+    @apply my-2 text-yellow-400;
+  }
 </style>
